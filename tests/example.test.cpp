4e2bd0f4d395ebe5d6c374424010adc20332b61c
comau <luibass92@live.it>
2021-12-18 23:42:13 +0100

    style: delete useless commented lines

--- tests/example.test.cpp
+++ tests/example.test.cpp
@@ -15,12 +15,6 @@ unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
 }
 
-//TEST_CASE("JSON-RPC", "[General tests]")
-//{
-//	jsonrpcpp::entity_ptr entity =	jsonrpcpp::Parser::do_parse(R"({"jsonrpc":"2.0","method" : "single_read","id" : "123","params" : {"pkts":1,"resTopic" : "res","variables" : [{"name":"var1","address" : "<ADDRESS_STRING>","dataType" : "<DATA_TYPE>"}]}})");
-//	jsonrpcpp::request_ptr request = std::dynamic_pointer_cast<jsonrpcpp::Request>(entity);
-//	decodeRead(request);
-//}
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(1) == 1 );
@@ -29,8 +23,3 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(10) == 3628800 );
 }
 
-//TEST_CASE("Configurations", "[Read Configuration File]") {
-//	// read configuration
-//    structs::Config l_config = io::config::getConfig();
-//	REQUIRE(l_config.basicTopic == "comau/smartopc");
-//}
